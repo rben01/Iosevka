3cb9f90b60a58f1863b76a3e234209f3dd65dbca
Robert Bennett <rltbennett@icloud.com>
2023-03-09 10:01:42 -0500

    Added -alt to build.sh (not neceessary for github actions, but still good to have things in sync) Removed partial derivative from -alt in build plan

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 npm install
-caffeinate npm run build -- --jCmd=3 {ttf-unhinted,woff2}"::"{iosevka-rltb-proportional-sans,iosevka-rltb-mono}
+caffeinate npm run build -- --jCmd=3 {ttf-unhinted,woff2}"::"{iosevka-rltb-proportional-sans,iosevka-rltb-mono,iosevka-rltb-mono-alt}
