0c56d0f76472f764fb75a5076fd86c789986891a
Robert Bennett <rltbennett@icloud.com>
2023-04-24 10:58:15 -0400

    Fixed quoting bug in make_woff2.py Made woff2 come before ttf in CSS Removed generation of ttf (redundant with webfont) Removed CSS files other than the one generated by Iosevka itself

--- make_woff2.py
+++ make_woff2.py
@@ -7,7 +7,7 @@ SUBSET_ARGS = [
     "--flavor=woff2",
     "--layout-features=kern,liga,clig,calt",
     # pretty much everything you'd need for Latin-esque stuff
-    '--unicodes="U+0020-007F,U+00A0-00FF,U+0100-017F,U+2000-206F,U+2070-209F,U+2200-22FF"',
+    "--unicodes=U+0020-007F,U+00A0-00FF,U+0100-017F,U+2000-206F,U+2070-209F,U+2200-22FF",
 ]
 
 
